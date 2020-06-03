2d07dbdbcb6f37e629cde5673896252d0d5030e9
Preston Shipman <pshipman2000@gmail.com>
2020-06-03 16:49:43 -0700

    work in progress

---
+++ caesers-cipher.js
@@ -0,0 +1,11 @@
+function rot13(str) {
+  let acsTwoStr = str.charCodeAt()
+  //  const index = 13;
+  //  let str1 = str.charCodeAt();
+  //  const str2 = str1.fromCharCode(str1)
+  //  console.log(str1)
+  let englishStr = str.fromCharCodeAt(13)
+  console.log(acsTwoStr)
+}
+
+rot13("SERR PBQR PNZC");
